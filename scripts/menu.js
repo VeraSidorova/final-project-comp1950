258635b3331f0a93f32d87425c864657497c0404
Shivam Mathur <info@bcit.ca>
2019-11-19 21:03:10 -0800

    Fix form code and update schedule page

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,10 +11,7 @@ const current_student = document.getElementById('current_student');
 const prospective_student = document.getElementById('prospective_student');
 
 const contact_form = document.getElementById('contact_form');
-const contact_submit_btn = document.getElementById('contact_submit_btn');
-
 const login_form = document.getElementById('login_form');
-const login_submit_btn = document.getElementById('login_submit_btn'); 
 
 // Click Handler
 btn_menu.addEventListener('click', _ => { body.classList.toggle('show') });
@@ -32,25 +29,14 @@ prospective_student.addEventListener('change', _ => {
 })
 
 // Contact Form Submission
-contact_form.addEventListener('submit', (e) => { e.preventDefault(); });
-contact_submit_btn.addEventListener("click", _ => {
-    handle_form_submission(contact_form, "Form Submitted! Thank you");
-})
+contact_form.addEventListener('submit', (e) => { 
+    e.preventDefault();
+    alert("Form Submitted! Thank you");
+    contact_form.reset();
+});
 
-// Login Form Submission
-login_form.addEventListener('submit', (e) => { e.preventDefault(); });
-login_submit_btn.addEventListener("click", _ => {
-    handle_form_submission(login_form, "Login Successful!");
-})
-
-function handle_form_submission(myForm, successMsg) {
-    form_inputs = myForm.getElementsByTagName('input');
-    for (let i = 0; i < form_inputs.length; i++) {
-        if (!form_inputs[i].checkValidity()) {
-            alert("Please fill the form correctly");
-            return;
-        }
-    }
-    alert(successMsg);
-    myForm.reset();
-}
+login_form.addEventListener('submit', (e) => { 
+    e.preventDefault(); 
+    alert("Login Successful!");
+    login_form.reset();
+});
